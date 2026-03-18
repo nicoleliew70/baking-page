fdcdbb77e50e07e78e61ceb97c356d6c9c8f0fb4
tnhmb <tnhmb@hotmail.com>
2026-03-18 12:43:32 +0800

    fix: Update sitemap base URL from nicolebaking.com to chefnicle.pages.dev.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nicolebaking.com';
+  const baseUrl = 'https://chefnicole.pages.dev';
 
   return [
     {
